100803dbc77d1facbea67f1760714b1f1b863242
wxie <wei.xie@cern.ch>
2019-05-24 17:05:48 -0400

    modualized the functions to make it more flexible to use

--- output/ana.C
+++ output/ana.C
@@ -2,8 +2,13 @@
 
 void ana(string dir="")
 {
-    production pd(dir);
-    pd.read_binary_data();
+    production pd;
+    pd.SetIntputBinaryFile(dir);
+    pd.read_in_SpikeTimes_data();
+    pd.read_SpikeTimes_data();
+    pd.read_Synapses_data();
+
+    pd.set_max_number_of_connections_per_pair(1);
     pd.find_PG();
 }
 
